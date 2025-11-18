ca990c35e57c7da0013a003d932b522cdde984a0
RAFAELLA GOMES DA SILVA <53341096841@imaccommand001.local>
2025-11-18 12:05:34 -0300

    exercicio03

--- exercicio03.js
+++ exercicio03.js
@@ -1,2 +1,8 @@
 // Faça um programa que receba um número do usuário e informe se este número é par ou
 // ímpar.
+ let numero = parseInt(prompt("informe um numero inteiro:"))
+ if(numero%2 == 0){
+    console.log ("o numero é par")
+ }else{
+    console.log( "o numero é impar")
+ }
