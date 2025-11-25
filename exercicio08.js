abb8f500940e1b1699c93f5c3691bcbaf143ad49
gomes <rafagomessilvaa@gmail.com>
2025-11-25 14:15:14 -0300

    Add files via upload

---
+++ exercicio08.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+
+let idade = parseInt(prompt("digite uma idade:"))
+
+while(idade < 18){
+    idade = parseInt(prompt("voce é menor de idade,digite outra idade:"))
+}
+
+console.log("voce é maior de idade,sua idade é" + idade)
